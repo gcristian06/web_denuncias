25e7a2fc813aa6575dca659002e724cd31392872
Cristian <166972078+gcristian06@users.noreply.github.com>
2025-06-04 02:04:44 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("consultarBtn").addEventListener("click", function() {
+    alert("Función de CONSULTAR en desarrollo.");
+});
+
+document.getElementById("loginBtn").addEventListener("click", function() {
+    alert("Redirigiendo a LOGIN...");
+    // window.location.href = "login.html";  // Si tienes una página de login
+});
+
+document.getElementById("denunciaBtn").addEventListener("click", function() {
+    alert("Formulario de DENUNCIA en desarrollo.");
+});
+
+document.getElementById("denunciaAnonimaBtn").addEventListener("click", function() {
+    alert("Formulario de DENUNCIA ANÓNIMA en desarrollo.");
+});
